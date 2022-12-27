e96ec6067e4cc2c2838134b3ae017e776b322206
Yusuf Tangaloğlu <yusuf.tangaloglu.5@gmail.com>
2022-12-28 00:27:33 +0300

    yusuf-blog v1.0

---
+++ components/Navbar.vue
@@ -0,0 +1,217 @@
+<template>
+    <section>
+        <!-- header -->
+        <header id="masthead" class="site-header" role="banner">
+ 			
+            
+             <!-- site-navigation -->
+             <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+                 
+                 
+                 
+                 <!-- layout-medium -->
+                 <div class="layout-medium">
+                 
+                     
+                     
+                     
+                     <!-- site-title : image-logo -->
+                     <h1 class="site-title">
+                         <a href="index.html" rel="home">
+                             <img src="images/site/logo.png" alt="logo">
+                             <span class="screen-reader-text">Haley Dust</span>
+                         </a>
+                     </h1>
+                     <!-- site-title -->
+                     
+                     <!-- site-title : text-logo -->
+                     <!--<h1 class="site-title">
+                         <a href="../index.html" rel="home">
+                             Haley Dust
+                         </a>
+                     </h1>-->
+                     <!-- site-title -->
+                     
+                     
+                     
+                 
+                     <a class="menu-toggle"><span class="lines"></span></a>
+                     
+                     <!-- nav-menu -->
+                     <div class="nav-menu">
+                         <ul>
+                             <li><a href="index.html">HOME</a>
+                                 <!-- level 2 -->
+                                 <ul>
+                                     <li><a href="blog-bold-with-sidebar.html">Blog Bold</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-bold.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-irregular-with-sidebar.html">Blog Irregular</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-irregular.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-creative-with-sidebar.html">Blog Creative</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-creative.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-waterfall-with-sidebar.html">Blog Waterfall</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-waterfall.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-regular-with-sidebar.html">Blog Regular</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-regular.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-grid-with-sidebar.html">Blog Grid</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-grid.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-list-with-sidebar.html">Blog List</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-list.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-creative-first-full-with-sidebar.html">1st Full + Creative</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-creative-first-full.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-irregular-first-full-with-sidebar.html">1st Full + Irregular</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-irregular-first-full.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-grid-first-full-with-sidebar.html">1st Full + Grid</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-grid-first-full.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-list-first-full-with-sidebar.html">1st Full + List</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-list-first-full.html">Full Width</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                 </ul>
+                                 <!-- level 2 -->
+                             </li>
+                             <li><a href="typo.html">Features</a>
+                                 <!-- level 2 -->
+                                 <ul>
+                                     <li><a href="blog-list-first-full-with-sidebar.html">Header Styles</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="index.html">Default</a></li>
+                                             <li><a href="blog-regular-with-sidebar.html">Classic Light</a></li>
+                                             <li><a href="blog-regular.html">Classic Dark</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="blog-single.html">Single Post</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="blog-single.html">Standart Post</a></li>
+                                             <li><a href="blog-single-with-sidebar.html">Post with sidebar</a></li>
+                                             <li><a href="blog-single-classic.html">Classic Post</a></li>
+                                             <li><a href="blog-single-video.html">Video Post</a></li>
+                                             <li><a href="blog-single-audio.html">Audio Post</a></li>
+                                             <li><a href="blog-single-gallery.html">Gallery Post</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                     <li><a href="resume.html">Resume</a></li>
+                                     <li><a href="typo.html">Typography</a></li>
+                                     <li><a href="shortcodes.html">Shortcodes</a></li>
+                                     <li><a href="page-full.html">Page Full Width</a></li>
+                                     <li><a href="#">Others</a>
+                                         <!-- level 3 -->
+                                         <ul>
+                                             <li><a href="grid.html">Grid</a></li>
+                                             <li><a href="form-elements.html">Forms</a></li>
+                                             <li><a href="404.html">404 Page</a></li>
+                                         </ul>
+                                         <!-- level 3 -->
+                                     </li>
+                                 </ul>
+                                 <!-- level 2 -->
+                             </li>
+                             <li><a href="cat-archives.html">ADVENTURE</a></li>
+                             <li><a href="cat-archives.html">LIFE</a></li>
+                             <li><a href="about.html">ABOUT ME</a></li>
+                             <li><a href="contact.html">CONTACT</a></li>
+                         </ul>
+                     </div>
+                     <!-- nav-menu -->
+                     
+                     <a class="search-toggle toggle-link"></a>
+                                         
+                     <!-- search-container -->
+                     <div class="search-container">
+                         
+                         <div class="search-box" role="search">
+                             <form method="get" class="search-form" action="#">
+                                 <label>Search Here
+                                     <input type="search" id="search-field" placeholder="type and hit enter" name="s">
+                                 </label>
+                                 <input type="submit" class="search-submit" value="Search">
+                             </form>
+                         </div>
+                     
+                     </div>
+                     <!-- search-container -->
+                     
+                     <!-- social-container -->
+                     <div class="social-container">
+                         
+                         <a class="social-link facebook" href="#"></a>
+                         <a class="social-link twitter" href="#"></a>
+                         <a class="social-link vine" href="#"></a>
+                         <a class="social-link dribbble" href="#"></a>
+                         <a class="social-link instagram" href="#"></a>
+     
+                     </div>
+                     <!-- social-container -->
+         
+                 </div>
+                 <!-- layout-medium -->
+                 
+             </nav>
+             <!-- site-navigation -->
+             
+             
+             
+             
+             
+                     
+         </header>
+         <!-- header -->
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="page" class="hfeed site">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+export default {
+
+  components: {
+    Navbar: () => import('@/components/Navbar'),
+  },
+
+  name: 'IndexPage'
+}
+</script>
