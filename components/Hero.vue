332ffcb720a0c5ea93e8c43546d6d0d1d9efb6a9
Yusuf Tangaloğlu <yusuf.tangaloglu.5@gmail.com>
2022-12-28 17:54:42 +0300

    update

---
+++ components/Hero.vue
@@ -0,0 +1,60 @@
+<template>
+    <section>
+         <!-- site-main -->
+        
+   
+                    <!-- site-content -->
+                    <div id="content" class="site-content" role="main"> <!-- .hentry -->
+                        <article class="hentry page">
+                            
+                                
+                            <!-- .entry-content -->
+                            <div class="entry-content intro" data-animation="rotate-1">
+                                
+                                
+                                
+                                <!-- .profile-image -->
+                                <div class="profile-image">
+                                	<img alt="profile" src="images/site/about-me-2.jpg"/>
+                                </div>  
+                                <!-- .profile-image -->
+                                
+                                
+                                <h2><em>Hi.</em> I am Haley Duster</h2>
+                                <h3>I am a <strong>blogger</strong> <strong>traveler</strong> <strong>writer</strong></h3>
+                                
+                                
+                                <!-- .link-boxes -->
+                                <figure>
+                                	<a href="about.html"><img src="images/site/box-01.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">About Me</figcaption>
+                                </figure>        
+                                
+                                <figure>
+                                	<a href="about.html"><img src="images/site/box-02.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Life</figcaption>
+                                </figure> 
+                                
+                                <figure>
+                                	<a href="about.html"><img src="images/site/box-03.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Travel</figcaption>
+                                </figure> 
+                                
+                                <figure>
+                                	<a href="https://www.instagram.com/pixelwarsdesign/"><img src="images/site/box-04.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Follow On Instagram</figcaption>
+                                </figure> 
+                                <!-- .link-boxes -->         
+                                	
+                                    
+                             </div> 
+                             <!-- .entry-content -->
+                                
+                                	
+                                    
+                         </article> 
+                         <!-- .page -->
+                    </div>
+               
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="page" class="hfeed site">
     <Navbar />
+    <div id="main" class="site-main">
+      <div class="layout-medium"> 
+        <div id="primary" class="content-area">
+          <Hero />
+          <PostSlider />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
 
   components: {
-    Navbar: () => import('@/components/Navbar'),
-  },
+    Navbar: () => import("@/components/Navbar.vue"),
+    Hero: () => import("@/components/Hero.vue"),
+    PostSlider: () => import("@/components/PostSlider.vue"),
+},
 
   name: 'IndexPage'
 }
